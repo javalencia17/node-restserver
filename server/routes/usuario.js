a841a975f375e76a3ee3838d471e513915844e4b
jhonatan_Syqual10 <jhonatan.alzate@syqual10.com>
2019-03-06 09:06:49 -0500

    login normal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -49,7 +49,7 @@ app.get('/usuario', verificaToken ,(req, res) => {
   
 });
 
-app.post('/usuario',  [verificaToken, verificaAdmin_Role],  (req, res) => {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let body =  req.body;
 
@@ -79,7 +79,7 @@ app.post('/usuario',  [verificaToken, verificaAdmin_Role],  (req, res) => {
   
 });
 
-app.put('/usuario/:id', verificaToken, function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function (req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body,['nombre', 'email','img','role','estado']);
@@ -102,7 +102,7 @@ app.put('/usuario/:id', verificaToken, function (req, res) {
    
 })
 
-app.delete('/usuario/:id',verificaToken, function (req, res) {
+app.delete('/usuario/:id',[verificaToken, verificaAdmin_Role], function (req, res) {
     
     let id = req.params.id;
     let cambiaEstado = {
